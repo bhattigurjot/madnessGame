7dd6e6d44da4efa23549e63869f60fccd102d0ce
Faheem Abrar <faheemabrar2003@gmail.com>
2017-05-20 17:41:52 -0600

    + Added enemy, waypoints, and their associated controllers

---
+++ Assets/Scripts/EnemyController/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public bool isFacingLeft { get; set; }
+    public float maxSpeed = 10.0f;
+
+    //Animator
+    Animator anim;
+    Animation animation;
+
+    // Use this for initialization
+    void Start () {
+        isFacingLeft = true;
+	}
+    
+    void FixedUpdate()
+    {        
+        if (GetComponent<Rigidbody2D>() != null)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2((isFacingLeft ? -1 : 1) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+
+    public void playerFlip()
+    {        
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
